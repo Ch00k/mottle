633e781a1fc04bbc79dd4047afdd1a25634992b9
Andrii Yurchuk <ay@mntw.re>
2024-05-27 09:22:47 +0200

    Allow running update job for specific user and playlist

--- web/jobs.py
+++ web/jobs.py
@@ -1,107 +1,116 @@
 import itertools
 import logging
-from collections import defaultdict
 
 from django.db.models import Q
 
 from web.email import send_email
-from web.spotify import get_client_token
 from web.views_utils import compile_email
 
-from .models import PlaylistUpdate, SpotifyUser, generate_playlist_update_hash
+from .models import Playlist, PlaylistUpdate, SpotifyUser, generate_playlist_update_hash
 from .utils import MottleException, MottleSpotifyClient
 
 logger = logging.getLogger(__name__)
 
 
-async def check_user_playlists_for_updates(user: SpotifyUser, spotify_client: MottleSpotifyClient) -> None:
-    updates = defaultdict(list)
+async def check_playlist_for_updates(playlist: Playlist, spotify_client: MottleSpotifyClient) -> list[PlaylistUpdate]:
+    updates: list[PlaylistUpdate] = []
 
-    async for playlist in user.playlists.filter(~Q(watched_playlists=None)):  # pyright: ignore
-        logger.info(f"Processing playlist {playlist}")
+    logger.info(f"Processing playlist {playlist}")
+
+    try:
+        playlist_tracks = await spotify_client.get_playlist_items(playlist.spotify_id)
+    except MottleException as e:
+        logger.error(f"Failed to get tracks for playlist {playlist}: {e}")
+        return updates
+
+    playlist_track_ids = set([track.track.id for track in playlist_tracks if track.track is not None])
+
+    watched_playlists = playlist.watched_playlists.all()
+    logger.info(f"Watched playlists: {await watched_playlists.acount()}")
+
+    async for watched_playlist in watched_playlists:
+        logger.info(f"Processing watched playlist {watched_playlist}")
 
         try:
-            playlist_tracks = await spotify_client.get_playlist_items(playlist.spotify_id)
+            watched_playlist_tracks = await spotify_client.get_playlist_items(watched_playlist.spotify_id)
         except MottleException as e:
-            logger.error(f"Failed to get tracks for playlist {playlist}: {e}")
+            logger.error(f"Failed to get tracks for watched playlist {watched_playlist}: {e}")
             continue
 
-        playlist_track_ids = set([track.track.id for track in playlist_tracks if track.track is not None])
+        watched_playlist_track_ids = set(
+            [track.track.id for track in watched_playlist_tracks if track.track is not None]
+        )
+        new_track_ids = watched_playlist_track_ids - playlist_track_ids
 
-        watched_playlists = playlist.watched_playlists.all()
-        logger.info(f"Watched playlists: {await watched_playlists.acount()}")
+        if not new_track_ids:
+            logger.info(f"No new tracks in playlist {watched_playlist}")
+            continue
 
-        async for watched_playlist in watched_playlists:
-            logger.info(f"Processing watched playlist {watched_playlist}")
+        logger.info(f"New track IDs: {new_track_ids}")
+        update_hash = generate_playlist_update_hash(tracks_added=list(new_track_ids))  # pyright: ignore
+        logger.debug(f"PlaylistUpdate hash: {update_hash}")
 
+        logger.info(f"Checking if PlaylistUpdate with hash {update_hash} already exists")
+        try:
+            update = await PlaylistUpdate.objects.aget(update_hash=update_hash)
+        except PlaylistUpdate.DoesNotExist:
+            logger.info(f"PlaylistUpdate with hash {update_hash} does not exist")
+            logger.info("Checking if PlaylistUpdate with the same target_playlist and source_playlist already exists")
             try:
-                watched_playlist_tracks = await spotify_client.get_playlist_items(watched_playlist.spotify_id)
-            except MottleException as e:
-                logger.error(f"Failed to get tracks for watched playlist {watched_playlist}: {e}")
-                continue
-
-            watched_playlist_track_ids = set(
-                [track.track.id for track in watched_playlist_tracks if track.track is not None]
-            )
-            new_track_ids = watched_playlist_track_ids - playlist_track_ids
-
-            if not new_track_ids:
-                logger.info(f"No new tracks in playlist {watched_playlist}")
-                continue
-
-            logger.info(f"New track IDs: {new_track_ids}")
-            update_hash = generate_playlist_update_hash(tracks_added=list(new_track_ids))  # pyright: ignore
-            logger.debug(f"PlaylistUpdate hash: {update_hash}")
-
-            logger.info(f"Checking if PlaylistUpdate with hash {update_hash} already exists")
-            try:
-                update = await PlaylistUpdate.objects.aget(update_hash=update_hash)
+                outdated_update = await PlaylistUpdate.objects.aget(
+                    target_playlist=playlist, source_playlist=watched_playlist, is_overridden_by=None
+                )
             except PlaylistUpdate.DoesNotExist:
-                logger.info(f"PlaylistUpdate with hash {update_hash} does not exist")
-                logger.info(
-                    "Checking if PlaylistUpdate with the same target_playlist and source_playlist " "already exists"
-                )
-                try:
-                    outdated_update = await PlaylistUpdate.objects.aget(
-                        target_playlist=playlist, source_playlist=watched_playlist, is_overridden_by=None
-                    )
-                except PlaylistUpdate.DoesNotExist:
-                    logger.info("PlaylistUpdate with the same target_playlist and source_playlist does not exist")
-                    outdated_update = None
-                else:
-                    logger.info(
-                        "PlaylistUpdate with the same target_playlist and source_playlist exists: {outdated_update}"
-                    )
-
-                update = await PlaylistUpdate.objects.acreate(
-                    target_playlist=playlist,
-                    source_playlist=watched_playlist,
-                    is_notified_of=False,
-                    is_accepted=None,
-                    tracks_added=list(new_track_ids),
-                )
-                logger.info(f"Created new PlaylistUpdate: {update}")
-
-                if outdated_update is not None:
-                    logger.info(f"Setting is_overridden_by to {update} for outdated {outdated_update}")
-                    outdated_update.is_overridden_by = update
-                    await outdated_update.asave()
-
-                updates[playlist.spotify_id].append(update)
+                logger.info("PlaylistUpdate with the same target_playlist and source_playlist does not exist")
+                outdated_update = None
             else:
-                logger.info(f"PlaylistUpdate with hash {update_hash} already exists: {update}")
-                if update.is_notified_of:
-                    logger.info(f"PlaylistUpdate {update} already has been notified of")
-                    continue
-                else:
-                    logger.info(f"PlaylistUpdate {update} has not been notified of yet")
-                    updates[playlist.spotify_id].append(update)
+                logger.info(
+                    f"PlaylistUpdate with the same target_playlist and source_playlist exists: {outdated_update}"
+                )
+
+            update = await PlaylistUpdate.objects.acreate(
+                target_playlist=playlist,
+                source_playlist=watched_playlist,
+                is_notified_of=False,
+                is_accepted=None,
+                tracks_added=list(new_track_ids),
+            )
+            logger.info(f"Created new PlaylistUpdate: {update}")
+
+            if outdated_update is not None:
+                logger.info(f"Setting is_overridden_by to {update} for outdated {outdated_update}")
+                outdated_update.is_overridden_by = update
+                await outdated_update.asave()
+
+            updates.append(update)
+        else:
+            logger.info(f"PlaylistUpdate with hash {update_hash} already exists: {update}")
+            if update.is_notified_of:
+                logger.info(f"PlaylistUpdate {update} already has been notified of")
+                continue
+            else:
+                logger.info(f"PlaylistUpdate {update} has not been notified of yet")
+                updates.append(update)
+
+    return updates
+
+
+async def check_user_playlists_for_updates(
+    user: SpotifyUser, spotify_client: MottleSpotifyClient, send_notifications: bool = False
+) -> None:
+    updates = {}
+
+    async for playlist in user.playlists.filter(~Q(watched_playlists=None)):  # pyright: ignore
+        playlist_updates = await check_playlist_for_updates(playlist, spotify_client)
+
+        if playlist_updates:
+            updates[playlist.spotify_id] = playlist_updates
 
     if updates:
-        if user.email is not None:
+        if send_notifications and user.email is not None:
             message = await compile_email(updates, spotify_client)
             logger.info(f"Sending email to {user.email}:\n{message}")
-            await send_email(user.email, "Playlists you watch got updates", message)
+            await send_email(user.email, "Your watched playlists have been updated", message)
 
             playlist_updates = list(itertools.chain.from_iterable(updates.values()))
             for pl in playlist_updates:
@@ -114,11 +123,9 @@ async def check_user_playlists_for_updates(user: SpotifyUser, spotify_client: Mo
         logger.info(f"No updates for user {user}")
 
 
-async def check_playlists_for_updates() -> None:
+async def check_playlists_for_updates(spotify_client: MottleSpotifyClient, send_notifications: bool = False) -> None:
     logger.info("Checking playlists for updates")
-    token = get_client_token()
-    spotify_client = MottleSpotifyClient(token.access_token)
 
     async for user in SpotifyUser.objects.filter(~Q(playlists=None)):
         logger.info(f"Processing user {user}")
-        await check_user_playlists_for_updates(user, spotify_client)
+        await check_user_playlists_for_updates(user, spotify_client, send_notifications)
--- web/management/commands/get_playlist_updates.py
+++ web/management/commands/get_playlist_updates.py
@@ -1,15 +1,51 @@
 import logging
+from functools import partial
+from typing import Any
 
 from asgiref.sync import async_to_sync
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
-from web.jobs import check_playlists_for_updates
+from web.jobs import check_playlist_for_updates, check_playlists_for_updates, check_user_playlists_for_updates
+from web.models import Playlist, SpotifyUser
+from web.spotify import get_client_token
+from web.utils import MottleSpotifyClient
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Runs all periodic jobs (async)"
+    help = "Check playlists for updates"
 
-    def handle(self, *_: tuple, **__: dict) -> None:
-        async_to_sync(check_playlists_for_updates)()
+    def add_arguments(self, parser: Any) -> None:
+        parser.add_argument("--user-id", type=str, help="The ID of the user to check playlists for")
+        parser.add_argument("--playlist-id", type=str, help="The ID of the plylist to check")
+        parser.add_argument("--send-notifications", action="store_true", help="Send notifications for updates")
+
+    def handle(self, *_: tuple, **options: dict) -> None:
+        user_id = options.get("user_id")
+        playlist_id = options.get("playlist_id")
+        send_notifications = options.get("send_notifications", False)
+
+        token = get_client_token()
+        spotify_client = MottleSpotifyClient(token.access_token)
+
+        if user_id:
+            try:
+                user = SpotifyUser.objects.get(spotify_id=user_id)
+            except SpotifyUser.DoesNotExist:
+                raise CommandError("User does not exist")
+            if user.playlists is None:  # pyright: ignore
+                raise CommandError("User has no playlists")
+
+            async_to_sync(
+                partial(check_user_playlists_for_updates, user, spotify_client, send_notifications)  # pyright: ignore
+            )()
+        elif playlist_id:
+            try:
+                playlist = Playlist.objects.get(id=playlist_id)
+            except Playlist.DoesNotExist:
+                raise CommandError("Playlist does not exist")
+
+            async_to_sync(partial(check_playlist_for_updates, playlist, spotify_client))()
+        else:
+            async_to_sync(partial(check_playlists_for_updates, spotify_client, send_notifications))()  # pyright: ignore
