52632ae70ef2b3887f4e7ed675fe59c37eba0b45
Andrii Yurchuk <ay@mntw.re>
2024-09-03 21:31:41 +0200

    Enable Prometheus multiprocessing for taskrunner

--- taskrunner/run.py
+++ taskrunner/run.py
@@ -7,7 +7,7 @@ from typing import Any, Optional
 from wsgiref.simple_server import WSGIServer
 
 from django_q.cluster import Cluster
-from prometheus_client import start_http_server
+from prometheus_client import CollectorRegistry, multiprocess, start_http_server
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +15,17 @@ logger = logging.getLogger(__name__)
 class MottleCluster(Cluster):
     def __init__(self, *args: Any, **kwargs: dict) -> None:
         super().__init__(*args, **kwargs)
+        self.prometheus_registry = CollectorRegistry()
+        multiprocess.MultiProcessCollector(self.prometheus_registry)
+
         self.metrics_server: Optional[WSGIServer] = None
         self.metrics_server_thread: Optional[Thread] = None
+
         signal.signal(signal.SIGHUP, self.sig_handler)
 
     def start(self, metrics_server_host: str, metrics_server_port: int) -> None:
         self.metrics_server, self.metrics_server_thread = start_http_server(
-            port=metrics_server_port, addr=metrics_server_host
+            port=metrics_server_port, addr=metrics_server_host, registry=self.prometheus_registry
         )
         logger.info(f"Metrics server started at {metrics_server_host}:{metrics_server_port}")
 
