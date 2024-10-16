f7eff12f4b4e5ba4cba0e62261e9e3718cb28d85
Andrii Yurchuk <ay@mntw.re>
2024-10-16 12:55:03 +0200

    Set ALLOWED_HOSTS in environment

--- mottle/settings.py
+++ mottle/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DJANGO_DEBUG", False)
 
 # web is the name of the Docker Compose service, and is needed for Prometheus scraping target
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "web", "mottle.it", "www.mottle.it"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")  # ["127.0.0.1", "localhost", "web", "mottle.it", "www.mottle.it"]
 CSRF_TRUSTED_ORIGINS = ["https://mottle.it", "https://www.mottle.it"]
 SESSION_COOKIE_DOMAIN = env.str("SESSION_COOKIE_DOMAIN", None)
 # SESSION_COOKIE_AGE = 3_153_600_000  # 100 years
